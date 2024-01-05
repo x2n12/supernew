3b16bd150e5a025f7cc883c6502abf289a76baa6
x2n12 <147017566+x2n12@users.noreply.github.com>
2024-01-05 21:20:28 +0700

    Update lukchin.lua

--- lukchin.lua
+++ lukchin.lua
@@ -5,7 +5,7 @@ while getgenv().god and task.wait() do
             if game:GetService("Players").LocalPlayer.Character and game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart") then
                 game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.Position, ball.Position)
                 if game:GetService("Players").LocalPlayer.Character:FindFirstChild("Highlight") then
-                    game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = ball.CFrame * CFrame.new(0, 0, (ball.Velocity).Magnitude * -0.5)                   
+                    game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = ball.CFrame * CFrame.new(2, 2, (ball.Velocity).Magnitude * -0.5)                   
                 end
             end
         end
